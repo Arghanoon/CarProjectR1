736303347301753602933507408c92251506d0f1
kamalkss <kamalkss@DESKTOP-B4MCIGP>
2016-08-05 19:08:41 +0430

    Revert "1395-05-15 18:26"
    This reverts commit 75563c6a2ed251e7ab5c6ae8b6e057f71dcb6f54.

--- CarProject/App_Code/EXTS.cs
+++ CarProject/App_Code/EXTS.cs
@@ -31,17 +31,6 @@ namespace CarProject.App_Code
             else
                 dic[x] = dic[x].ToString() + " " + value.ToString();
         }
-        public static string DicAtrsToString(IDictionary<string, object> dic)
-        {
-            string res = "";
-
-            foreach (var item in dic)
-            {
-                res += string.Format(" {0}=\"{1}\" ", item.Key, item.Value.ToString());
-            }
-
-            return res;
-        }
 
         public static MvcHtmlString input_TextBox<tm, tp>(this HtmlHelper<tm> htmlHelper, Expression<Func<tm, tp>> expression, string displayName, IDictionary<string, object> htmlAttributes, string errorClass)
         {
@@ -112,20 +101,16 @@ namespace CarProject.App_Code
             if (!htmlHelper.ViewData.ModelState.IsValidField(htmlHelper.NameFor(expression).ToString()))
                 AddAttribute(htmlAttributes, "class", errorClass);
 
-            List<SelectListItem> items = new List<SelectListItem>();
-            items.Add(new SelectListItem { Text = "--انتخاب کنید--", Value = null, Selected = true });
-
-            foreach (var item in Items)
-	        {
-                items.Add(new SelectListItem { Text = item, Value = item });
+            string attributes = "";
+            foreach (var item in htmlAttributes)
+            {
+                attributes = string.Format(" {0}=\"{1}\" ", item.Key, item.Value.ToString());
             }
 
             string res = "<section class=\"input\">";
 
             res += htmlHelper.LabelFor(expression, displayName);
-            res += htmlHelper.DropDownListFor(expression, items, htmlAttributes);
-
-            /*res += string.Format("<select {0} {1} {2} >", htmlHelper.IdFor(expression).ToString(), htmlHelper.NameFor(expression).ToString(), attributes);
+            res += string.Format("<select {0} {1} {2} >", htmlHelper.IdFor(expression).ToString(), htmlHelper.NameFor(expression).ToString(), attributes);
            
             res += string.Format("<option value=\"\" >{0}</option>", "--انتخاب کنید--");
             foreach (var item in Items)
@@ -133,8 +118,7 @@ namespace CarProject.App_Code
                 res += string.Format("<option value=\"{0}\" >{0}</option>", item);
             }
             
-            res += "</select>";*/
-
+            res += "</select>";
             res += htmlHelper.ValidationMessageFor(expression);
 
             res += "</section>";
@@ -151,38 +135,6 @@ namespace CarProject.App_Code
         public static MvcHtmlString input_ComboBox<TM, TP>(this HtmlHelper<TM> htmlHelper, Expression<Func<TM, TP>> expres, string displayName, IList<string> Items, IDictionary<string, object> htmlAttributes)
         {
             return input_ComboBox<TM, TP>(htmlHelper, expres, displayName, Items, htmlAttributes, "error");
-        }
-
-
-        public static MvcHtmlString input_CheckBox<tm>(this HtmlHelper<tm> htmlHelper, Expression<Func<tm, bool?>> expression, string displayName, IDictionary<string, object> htmlAttributes, string errorClass)
-        {
-            if (!htmlHelper.ViewData.ModelState.IsValidField(htmlHelper.NameFor(expression).ToString()))
-                AddAttribute(htmlAttributes, "class", errorClass);
-
-           
-
-            string res = "<section class=\"input\">";
-
-            res += htmlHelper.LabelFor(expression, displayName);
-
-            res += htmlHelper.CheckBox(htmlHelper.NameFor(expression).ToString(), htmlAttributes);
-
-            res += htmlHelper.ValidationMessageFor(expression);
-
-            res += "</section>";
-            return new MvcHtmlString(res);
-        }
-        public static MvcHtmlString input_CheckBox<TM>(this HtmlHelper<TM> htmlHelper, Expression<Func<TM, bool?>> expres, string displayName, string errorClass)
-        {
-            return input_CheckBox<TM>(htmlHelper, expres, displayName, new Dictionary<string, object>(), errorClass);
-        }
-        public static MvcHtmlString input_CheckBox<TM>(this HtmlHelper<TM> htmlHelper, Expression<Func<TM, bool?>> expres, string displayName)
-        {
-            return input_CheckBox<TM>(htmlHelper, expres, displayName, new Dictionary<string, object>(), "error");
-        }
-        public static MvcHtmlString input_CheckBox<TM>(this HtmlHelper<TM> htmlHelper, Expression<Func<TM, bool?>> expres, string displayName, IDictionary<string, object> htmlAttributes)
-        {
-            return input_CheckBox<TM>(htmlHelper, expres, displayName, htmlAttributes, "error");
         }
 
     }
