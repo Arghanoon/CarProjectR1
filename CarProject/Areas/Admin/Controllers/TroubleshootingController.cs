78481b51ae9cd84cff0a18f5719edfca7bb3af72
Mahdi <Mahdi>
2016-08-17 11:48:57 +0430

    1395-05-27 11:48
    TroublesShooting Finished

--- CarProject/Areas/Admin/Controllers/TroubleshootingController.cs
+++ CarProject/Areas/Admin/Controllers/TroubleshootingController.cs
@@ -46,5 +46,98 @@ namespace CarProject.Areas.Admin.Controllers
 
             return View(tobj);
         }
+
+        public ActionResult Update(int? id)
+        {
+            var tob = new Models.Cars.TroubleshootingClass(id);
+            Session["troubleUpdate"] = tob;
+
+            return View(tob);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Models.Cars.TroubleshootingClass tob)
+        {
+            if (ViewData.ModelState.IsValid)
+            {
+                var stob = Session["troubleUpdate"] as Models.Cars.TroubleshootingClass;
+                this.TryUpdateModel(stob);
+                stob.Update();
+                return RedirectToAction("Index");
+            }
+            return View(tob);
+        }
+
+        public ActionResult DeleteTrouble(FormCollection form)
+        {
+            int id = -1;
+            int.TryParse(form["id"], out id);
+            if (id > 0)
+            {
+                var dc = new DBSEF.CarAutomationEntities();
+
+                if (form.AllKeys.Contains("RemoveAll"))
+                {
+                    DeleteTroubleBranche(id, dc);
+                }
+                else if (form.AllKeys.Contains("RemoveItem"))
+                {
+                    var dit = dc.Troubleshootings.FirstOrDefault(t => t.TroubleshootingId == id);
+
+                    if (dit != null)
+                    {
+                        var fid = dit.FatherId;
+                        dc.Troubleshootings.Remove(dit);
+                        var l = dc.Troubleshootings.Where(t => t.FatherId == id);
+                        foreach (var item in l)
+                        {
+                            item.FatherId = fid;
+                        }
+                    }
+                }
+                dc.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteAll(int id)
+        {
+            var dc = new DBSEF.CarAutomationEntities();
+            DeleteTroubleBranche(id, dc);
+            dc.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteItem(int id)
+        {
+            var dc = new DBSEF.CarAutomationEntities();
+            var dit = dc.Troubleshootings.FirstOrDefault(t => t.TroubleshootingId == id);
+
+            if (dit != null)
+            {
+                var fid = dit.FatherId;
+                dc.Troubleshootings.Remove(dit);
+                var l = dc.Troubleshootings.Where(t => t.FatherId == id);
+                foreach (var item in l)
+                {
+                    item.FatherId = fid;
+                }
+
+                dc.SaveChanges();
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+        void DeleteTroubleBranche(int id, DBSEF.CarAutomationEntities con)
+        {
+            var xt = con.Troubleshootings.FirstOrDefault(t => t.TroubleshootingId == id);
+            foreach (var item in con.Troubleshootings.Where(t => t.FatherId == id))
+            {
+                DeleteTroubleBranche(item.TroubleshootingId, con);
+            }
+            con.Troubleshootings.Remove(xt);
+        }
     }
 }
--- CarProject/Areas/Admin/Models/Cars/Troubleshooting.cs
+++ CarProject/Areas/Admin/Models/Cars/Troubleshooting.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Web;
 
 using dbs = CarProject.DBSEF;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarProject.Areas.Admin.Models.Cars
 {
-    public class TroubleshootingClass
+    public class TroubleshootingClass : IValidatableObject
     {
         dbs.CarAutomationEntities dbc = new dbs.CarAutomationEntities();
 
@@ -17,11 +18,30 @@ namespace CarProject.Areas.Admin.Models.Cars
         {
             Troubleshooting = new dbs.Troubleshooting();
         }
+        public TroubleshootingClass(Nullable<int> id)
+        {
+            Troubleshooting = dbc.Troubleshootings.FirstOrDefault(t => t.TroubleshootingId == id);
+        }
 
         public void Save()
         {
             dbc.Troubleshootings.Add(Troubleshooting);
             dbc.SaveChanges();
+        }
+        public void Update()
+        {
+            dbc.SaveChanges();
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrWhiteSpace(Troubleshooting.Question))
+                yield return new ValidationResult("سوال وارد نشده است", new string[] { "Troubleshooting.Question" });
+            if (string.IsNullOrWhiteSpace(Troubleshooting.Answer))
+                yield return new ValidationResult("جواب وارد نشده است", new string[] { "Troubleshooting.Answer" });
+
+            if (Troubleshooting.ServicePrice != null && Troubleshooting.ServicePrice > 0 && !(Troubleshooting.ProductId != null && Troubleshooting.ProductId > 0))
+                yield return new ValidationResult("فقط در صورت تعیین قطعه امکان تعیین قیمت وجود دارد", new string[] { "Troubleshooting.ServicePrice" });
         }
     }
 }
