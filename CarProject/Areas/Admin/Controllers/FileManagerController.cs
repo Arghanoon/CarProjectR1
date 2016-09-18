2a90b45f4a2532252e191bdcca05748933ff3fee
Mahdi <Mahdi>
2016-09-19 02:06:20 +0430

    1395-06-29 02:06

--- CarProject/Areas/Admin/Controllers/FileManagerController.cs
+++ CarProject/Areas/Admin/Controllers/FileManagerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using CarProject.App_Code;
 
 namespace CarProject.Areas.Admin.Controllers
 {
@@ -16,5 +18,25 @@ namespace CarProject.Areas.Admin.Controllers
             return View(model: path);
         }
 
+
+        public JsonResult GetImages(string path)
+        {
+            var rtdirel = "~/Publics/Filemanager";
+            if (path != null && !path.IsNullOrWhiteSpace())
+                rtdirel = Path.Combine(rtdirel, path).Replace('\\', '/');
+            
+            var dic = new DirectoryInfo(Server.MapPath(rtdirel));
+            List<dynamic> dirs = new List<dynamic>();
+            List<dynamic> images = new List<dynamic>();
+            foreach (var item in dic.GetDirectories())
+            {
+                dirs.Add(new { name = item.Name, path = Url.Content(Path.Combine(rtdirel, item.Name).Replace('\\', '/')) });
+            }
+            foreach (var item in dic.GetFiles())
+            {
+                images.Add(new { name = item.Name, path = Url.Content(Path.Combine(rtdirel, item.Name).Replace('\\', '/')) });
+            }
+            return Json(new { DIR = dirs, IMGS = images }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
