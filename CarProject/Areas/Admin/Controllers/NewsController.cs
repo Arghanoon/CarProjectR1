af48da1be5346668b541261beacde5318e662d45
Mahdi <Mahdi>
2016-09-21 16:50:53 +0330

    1395-06-31 16:50

---
+++ CarProject/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarProject.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        //
+        // GET: /Admin/News/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult News_Publish()
+        {
+            var model = new Models.News.Newsmodel();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult News_Publish(Models.News.Newsmodel model)
+        {
+            return View(model);
+        }
+
+    }
+}
---
+++ CarProject/Areas/Admin/Models/News/Newsmodel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using db = CarProject.DBSEF;
+
+namespace CarProject.Areas.Admin.Models.News
+{
+    public class Newsmodel : db.Content , IValidatableObject
+    {
+        [AllowHtml]
+        public string ContentHTML
+        {
+            get { return this.ContentText; }
+            set { this.ContentText = value; }
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> result = new List<ValidationResult>();
+
+            return result;
+        }
+    }
+}
