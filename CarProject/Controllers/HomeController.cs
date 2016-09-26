65fd846813de9bc2bf929bf06be2498a20f9498d
Mahdi <Mahdi>
2016-09-27 00:47:49 +0330

    1395-07-06 00:47

--- CarProject/Controllers/HomeController.cs
+++ CarProject/Controllers/HomeController.cs
@@ -31,15 +31,18 @@ namespace CarProject.Controllers
 
         public ActionResult ContectUs()
         {
+            ViewBag.success = "";
             var model = new Models.Home.ContactUsMessageModel();
             return View(model);
         }
         [HttpPost]
         public ActionResult ContectUs(Models.Home.ContactUsMessageModel model)
         {
+            ViewBag.success = "";
             if (ModelState.IsValid)
             {
                 model.Save();
+                ViewBag.success = "پیام شما ثبت گردید";
             }
             return View(model);
         }
