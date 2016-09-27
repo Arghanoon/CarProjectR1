a5187269354ebba32448b366287386f97aefde6f
kamalkss <kamalkss@hotmail.com>
2016-09-27 06:28:00 +0330

    23

--- CarProject/CLS/Searchs/CarsSearchLogic.cs
+++ CarProject/CLS/Searchs/CarsSearchLogic.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.Entity.Infrastructure;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
@@ -18,40 +19,78 @@ namespace CarProject.CLS.Searchs
         public CarsSearchLogic()
         {
             Contexts = new CarAutomationEntities();
+            
+            
         }
 
 
         public IEnumerable GetJoinedView(SearchModel search)
         {
-            //Car.Join(CarModel, p => , c => , (p, c) => new {p, c}) 
-            var Resultting = (Contexts.CarBrands.Join(Contexts.CarModels, carbrand => carbrand.CarBrandId,
-                carmodel => carmodel.CarBrandId, (carbrand, carmodel) => new { carbrand, carmodel })
-                .Join(Contexts.Cars, @t => @t.carmodel.CarModelId, cars => cars.CarModelId, (@t, cars) => new { @t, cars })
-                .Join(Contexts.CarGearBoxes, @t => @t.cars.CarsId, cargearbox => cargearbox.CarsId,
-                    (@t, cargearbox) => new { @t, cargearbox })
-                .Join(Contexts.CarEngines, @t => @t.@t.cars.CarsId, carengine => carengine.CarsId,
-                    (@t, carengine) => new { @t, carengine })
-                .Join(Contexts.CarPrices, @t => @t.@t.@t.cars.CarsId, carprice => carprice.CarsId,
-                    (@t, carprice) => new { @t, carprice })
-                .Join(Contexts.FuelConsumptions, @t => @t.@t.@t.@t.cars.CarsId, fuel => fuel.CarId, (@t, fuel) => new
-                {
-                    @t.@t.@t.@t.@t.carbrand.CarBrandName,
-                    @t.@t.@t.@t.@t.carmodel.CarModelName,
-                    @t.@t.carengine.EngineType,
-                    @t.@t.carengine.EngineCylinderNumber,
-                    @t.@t.@t.cargearbox.GearBoxType,
-                    @t.@t.@t.cargearbox.GearBoxAxel,
-                    @t.@t.@t.@t.cars.CarUsage,
-                    @t.@t.@t.@t.cars.CarYearModel,
-                    @t.@t.@t.@t.cars.CarBodyType,
-                    fuel.LphCity,
-                    fuel.LphRoad,
-                    fuel.LphMix
-                })).Distinct();
+            
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            
+            var rx = Contexts.sp_cars().Select(c => new
+            {
+                CarBrandnamex = c.CarBrandName,
+                CarModelNamex = c.CarModelName,
+                CarUsagex = c.CarUsage,
+                CarBodyTypex = c.CarBodyType,
+                GearBoxTypex = c.GearBoxType,
+                GearBoxAxelx = c.GearBoxAxel,
+                EngineTypex = c.EngineType,
+                EngineCylinderNumberx = (int?)c.EngineCylinderNumber,
+                CarYearModelx = (int?)c.CarYearModel,
+                LphCityx = (double?)c.LphCity,
+                LphRoadx = (double?)c.LphRoad,
+                LphMixx = (double?)c.LphMix
+            });
 
+            var list = rx.ToList();
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            var watch2 = System.Diagnostics.Stopwatch.StartNew();
+            //Car.Join(CarModel, p => , c => , (p, c) => new {p, c}) 
+            var Resultting = (Contexts.CarBrands.GroupJoin(Contexts.CarModels, CarBrand => CarBrand.CarBrandId,
+                CarModel => CarModel.CarBrandId, (CarBrand, CarModel_join) => new {CarBrand, CarModel_join})
+                .SelectMany(@t => @t.CarModel_join.DefaultIfEmpty(), (@t, CarModel) => new {@t, CarModel})
+                .GroupJoin(Contexts.Cars, @t => @t.CarModel.CarModelId, Cars => Cars.CarModelId,
+                    (@t, Cars_join) => new {@t, Cars_join})
+                .SelectMany(@t => @t.Cars_join.DefaultIfEmpty(), (@t, Cars) => new {@t, Cars})
+                .GroupJoin(Contexts.CarGearBoxes, @t => @t.Cars.CarsId, CarGearBox => CarGearBox.CarsId,
+                    (@t, CarGearBox_join) => new {@t, CarGearBox_join})
+                .SelectMany(@t => @t.CarGearBox_join.DefaultIfEmpty(), (@t, CarGearBox) => new {@t, CarGearBox})
+                .GroupJoin(Contexts.CarEngines, @t => @t.@t.@t.Cars.CarsId, CarEngine => CarEngine.CarsId,
+                    (@t, CarEngine_join) => new {@t, CarEngine_join})
+                .SelectMany(@t => @t.CarEngine_join.DefaultIfEmpty(), (@t, CarEngine) => new {@t, CarEngine})
+                .GroupJoin(Contexts.FuelConsumptions, @t => @t.@t.@t.@t.@t.Cars.CarsId,
+                    FuelConsumption => FuelConsumption.CarId,
+                    (@t, FuelConsumption_join) => new {@t, FuelConsumption_join})
+                .SelectMany(@t => @t.FuelConsumption_join.DefaultIfEmpty(), (@t, FuelConsumption) => new
+                {
+                    @t.@t.@t.@t.@t.@t.@t.@t.@t.CarBrand.CarBrandName,
+                    CarModelName = @t.@t.@t.@t.@t.@t.@t.@t.CarModel.CarModelName,
+                    CarUsage = @t.@t.@t.@t.@t.@t.Cars.CarUsage,
+                    CarBodyType = @t.@t.@t.@t.@t.@t.Cars.CarBodyType,
+                    GearBoxType = @t.@t.@t.@t.CarGearBox.GearBoxType,
+                    GearBoxAxel = @t.@t.@t.@t.CarGearBox.GearBoxAxel,
+                    EngineType = @t.@t.CarEngine.EngineType,
+                    EngineCylinderNumber = (int?) @t.@t.CarEngine.EngineCylinderNumber,
+                    CarYearModel = (int?) @t.@t.@t.@t.@t.@t.Cars.CarYearModel,
+                    LphCity = (double?) FuelConsumption.LphCity,
+                    LphRoad = (double?) FuelConsumption.LphRoad,
+                    LphMix = (double?) FuelConsumption.LphMix
+                })).Distinct();
+            watch2.Stop();
+            var elapsedMs2 = watch2.ElapsedMilliseconds;
+
+            var list2 = Resultting.ToList();    
             var result = Resultting;
+            
+
             var r1 = result.ToList();
-            var r2 = r1;
+            var r2 = result.ToList();
+
             var r3 = r2;
             var r4 = r3;
             var r5 = r4;
@@ -67,7 +106,7 @@ namespace CarProject.CLS.Searchs
             r7.Clear();
 
             rFinal.Clear();
-
+            
 
             //  var result = Resultting.ToList();
 
@@ -76,7 +115,7 @@ namespace CarProject.CLS.Searchs
                 foreach (var VARIABLE in search.brandname)
                 {
                     r1.AddRange(Resultting.Where(x => x.CarBrandName == VARIABLE));
-                    r2 = r1;
+                    r2.AddRange(r1);
                     r1.Clear();
                 }
             }
