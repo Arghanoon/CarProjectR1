5cd7515ad454090f6332b01beb62ad34537ee62d
kamalkss <kamalkss@hotmail.com>
2016-09-27 13:03:39 +0330

    qw

--- CarProject/CLS/Searchs/ServicesSearchModel.cs
+++ CarProject/CLS/Searchs/ServicesSearchModel.cs
@@ -7,5 +7,13 @@ namespace CarProject.CLS
 {
     public class ServicesSearchModel
     {
+        public string[] ProductName { get; set; }
+        public string[] BrandName { get; set; }
+        public string[] CarmodelName { get; set; }
+        public string[] Manufacture { get; set; }
+        public string[] Country { get; set; }
+        public int PriceFrom { get; set; }
+        public int PriceTo { get; set; }
+        public string[] CategoryName { get; set; }
     }
 }
