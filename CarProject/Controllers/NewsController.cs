0ed4a5df955a46a5785e426821e41e1f23edb811
Mahdi <Mahdi>
2016-09-29 00:25:59 +0330

    Signed-off-by: Mahdi <Mahdi>

--- CarProject/Controllers/NewsController.cs
+++ CarProject/Controllers/NewsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using CarProject.App_extension;
+
 namespace CarProject.Controllers
 {
     public class NewsController : Controller
@@ -16,5 +18,24 @@ namespace CarProject.Controllers
             return View();
         }
 
+    }
+
+    public class NewsGroupLink
+    {
+        public MvcHtmlString CreateGroupLink(int? parent, string navclass = "", string linkClass = "", int datalevel = 1)
+        {
+            string res = "";
+            var DBS = new DBSEF.CarAutomationEntities();
+
+            foreach (DBSEF.ContentsCategory item in DBS.ContentsCategories.Where(cc => cc.ParentId == parent))
+            {
+                res += string.Format("<a href=\"{0}\" class=\"\">{1}</a> ");
+                res += CreateGroupLink(item.ContentsCategoryId, navclass, linkClass, (datalevel + 1));
+            }
+
+            if (!res.IsNullOrWhiteSpace())
+                res = string.Format("<nav class=\"{0}\" data-level=\"{1}\">{2}</nav>", navclass, datalevel, res);
+            return new MvcHtmlString(res);
+        }
     }
 }
