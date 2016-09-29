ed739f3037f3c2435ebad65fc1999a56da9dcb09
Mahdi <Mahdi>
2016-09-29 12:45:13 +0330

    1395-07-08 12:45

--- CarProject/Controllers/NewsController.cs
+++ CarProject/Controllers/NewsController.cs
@@ -22,15 +22,25 @@ namespace CarProject.Controllers
 
     public class NewsGroupLink
     {
-        public MvcHtmlString CreateGroupLink(int? parent, string navclass = "", string linkClass = "", int datalevel = 1)
+        public MvcHtmlString CreateGroupLink(int? parent, string navclass = "", string linkClass = "", string isroot = "", int datalevel = 1)
         {
             string res = "";
             var DBS = new DBSEF.CarAutomationEntities();
 
             foreach (DBSEF.ContentsCategory item in DBS.ContentsCategories.Where(cc => cc.ParentId == parent))
             {
-                res += string.Format("<a href=\"{0}\" class=\"{1}\">{2}</a> ", "#", linkClass, item.Name);
-                res += CreateGroupLink(item.ContentsCategoryId, navclass, linkClass, (datalevel + 1));
+                res += "<section>";
+                {
+                    var tmp = CreateGroupLink(item.ContentsCategoryId, navclass, linkClass, isroot, (datalevel + 1));
+
+                    if (tmp.ToString().IsNullOrWhiteSpace())
+                        res += string.Format("<a href=\"{0}\" class=\"{1}\">{2}</a> ", "#", linkClass, item.Name);
+                    else
+                        res += string.Format("<a href=\"{0}\" class=\"{1}\">{2}</a> ", "#", string.Format("{0} {1}", linkClass, isroot), item.Name);
+
+                    res += tmp;
+                }
+                res += "</section>";
             }
 
             if (!res.IsNullOrWhiteSpace())
