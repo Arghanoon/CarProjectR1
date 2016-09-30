6c8266df995f64f8660d8a399b940d4cca55fbb8
Mahdi <Mahdi>
2016-09-30 15:35:55 +0330

    Signed-off-by: Mahdi <Mahdi>

---
+++ CarProject/Areas/Admin/Controllers/StoreController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarProject.Areas.Admin.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Admin/Store/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products_Insert()
+        {
+            return View();
+        }
+    }
+}
