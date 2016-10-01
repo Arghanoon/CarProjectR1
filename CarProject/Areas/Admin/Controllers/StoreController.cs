53893be0ca66cfdd90ac903017a4b997f4f87309
Mahdi <Mahdi>
2016-10-01 10:28:46 +0330

    Signed-off-by: Mahdi <Mahdi>

--- CarProject/Areas/Admin/Controllers/StoreController.cs
+++ CarProject/Areas/Admin/Controllers/StoreController.cs
@@ -11,14 +11,25 @@ namespace CarProject.Areas.Admin.Controllers
         //
         // GET: /Admin/Store/
 
+        
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult CategoryManagment()
+        public ActionResult CategoryManagment(int? id)
         {
-            return View();
+            var model = new Models.Store.CategoryModel(id);
+            return View(model);
+        }
+        public ActionResult CategoryManagment(Models.Store.CategoryModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Save();
+                return RedirectToAction("CategoryManagment", new { id = model.Category.ParentCategoryId });
+            }
+            return View(model);
         }
 
         public ActionResult Products_Insert()
---
+++ CarProject/Areas/Admin/Models/Store/Category.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarProject.Areas.Admin.Models.Store
+{
+    public class CategoryModel : IValidatableObject
+    {
+        public DBSEF.CarAutomationEntities DBS = new DBSEF.CarAutomationEntities();
+        public DBSEF.Category Category { get; set; }
+
+        public CategoryModel()
+        {
+            Category = new DBSEF.Category();
+        }
+        public CategoryModel(int? id)
+        {
+            Category = DBS.Categories.FirstOrDefault(c => c.CategoryId == id);
+        }
+
+        public void Save()
+        {
+            DBS.Categories.Add(this.Category);
+            DBS.SaveChanges();
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> result = new List<ValidationResult>();
+
+            return result;
+        }
+    }
+}
