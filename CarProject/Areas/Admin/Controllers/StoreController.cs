650e893f36bb2a12074a133413ab64e391bf8025
Mahdi <Mahdi>
2016-10-01 16:13:21 +0330

    Signed-off-by: Mahdi <Mahdi>

--- CarProject/Areas/Admin/Controllers/StoreController.cs
+++ CarProject/Areas/Admin/Controllers/StoreController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using CarProject.App_extension;
+
 namespace CarProject.Areas.Admin.Controllers
 {
     public class StoreController : Controller
@@ -22,6 +24,7 @@ namespace CarProject.Areas.Admin.Controllers
             var model = new Models.Store.CategoryModel(id);
             return View(model);
         }
+        [HttpPost]
         public ActionResult CategoryManagment(Models.Store.CategoryModel model)
         {
             if (ModelState.IsValid)
@@ -35,6 +38,32 @@ namespace CarProject.Areas.Admin.Controllers
         public ActionResult Products_Insert()
         {
             return View();
+        }
+    }
+
+    public class StoreMVC
+    {
+        DBSEF.CarAutomationEntities DBS = new DBSEF.CarAutomationEntities();
+        public MvcHtmlString Categories(int? pid, int level = 1, string ulclass = "",string liClass = "", string aHref = "#", string aClass = "")
+        {
+            string res = "";
+
+            foreach (var item in DBS.Categories.Where(c => c.ParentCategoryId == pid) )
+            {
+                res += string.Format("<li class=\"{0}\" >", liClass);
+                {
+                    var tmp = Categories(item.CategoryId, (level + 1), ulclass, liClass, aHref, aClass);
+                    res += string.Format("<a href=\"{0}\" class=\"{1}\" data-id=\"{2}\" data-name=\"{3}\" data-describe=\"{4}\" >{5}</a> {6}",
+                        aHref, aClass, item.CategoryId, item.CategoryName,
+                        HttpUtility.HtmlEncode(item.Description), item.CategoryName, tmp);
+                }
+                res += "</li>";
+            }
+
+
+            if (!res.IsNullOrWhiteSpace())
+                res = string.Format("<ul class=\"{0}\"  data-level=\"{1}\" >{2}</ul>", ulclass, level, res);
+            return new MvcHtmlString(res);
         }
     }
 }
--- CarProject/Areas/Admin/Models/Store/Category.cs
+++ CarProject/Areas/Admin/Models/Store/Category.cs
@@ -4,6 +4,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+using CarProject.App_extension;
+
 namespace CarProject.Areas.Admin.Models.Store
 {
     public class CategoryModel : IValidatableObject
@@ -29,6 +31,10 @@ namespace CarProject.Areas.Admin.Models.Store
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             List<ValidationResult> result = new List<ValidationResult>();
+            if (Category.CategoryName.IsNullOrWhiteSpace())
+                result.Add(new ValidationResult("نام گروه وارد نشده است", new string[] { "Category.CategoryName" }));
+            if(DBS.Categories.Count(c => c.CategoryName == this.Category.CategoryName && c.ParentCategoryId == this.Category.ParentCategoryId && c.CategoryId != this.Category.CategoryId) > 0)
+                result.Add(new ValidationResult("نام گروه وارد شده تکراری است", new string[] { "Category.CategoryName" }));
 
             return result;
         }
