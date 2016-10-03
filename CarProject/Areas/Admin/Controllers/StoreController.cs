57d8914bc8fbe75a8013d5d0a4f0e42ab9bfe9ba
Mahdi <Mahdi>
2016-10-03 20:40:37 +0330

    Signed-off-by: Mahdi <Mahdi>

--- CarProject/Areas/Admin/Controllers/StoreController.cs
+++ CarProject/Areas/Admin/Controllers/StoreController.cs
@@ -36,6 +36,27 @@ namespace CarProject.Areas.Admin.Controllers
             return View(model);
         }
 
+        public ActionResult CategoryManagment_Update(int? id)
+        {
+            var model = new Models.Store.CategoryModel(id);
+            TempData["updateStoreCategoryModel"] = model;
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult CategoryManagment_Update(Models.Store.CategoryModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var m = TempData["updateStoreCategoryModel"] as Models.Store.CategoryModel;
+                TryUpdateModel(m);
+                m.DBS.SaveChanges();
+
+                return RedirectToAction("CategoryManagment");
+            }
+            return View(model);
+        }
+
+
         public ActionResult Products_Insert()
         {
             return View();
@@ -45,21 +66,62 @@ namespace CarProject.Areas.Admin.Controllers
     public class StoreMVC
     {
         DBSEF.CarAutomationEntities DBS = new DBSEF.CarAutomationEntities();
-        public MvcHtmlString Categories(int? pid, int level = 1, string ulclass = "",string liClass = "", string aHref = "#", string aClass = "")
+        public MvcHtmlString Categories_readonly(int? pid, int level = 1, int? skipShoingId = null, string ulclass = "", string liClass = "", string aHref = "#", string aClass = "")
         {
             string res = "";
 
-            foreach (var item in DBS.Categories.Where(c => c.ParentCategoryId == pid) )
+            foreach (var item in DBS.Categories.Where(c => c.ParentCategoryId == pid))
             {
+                if (item.CategoryId == skipShoingId)
+                    continue;
+
                 res += string.Format("<li class=\"{0}\" >", liClass);
                 {
-                    var tmp = Categories(item.CategoryId, (level + 1), ulclass, liClass, aHref, aClass);
+                    var tmp = Categories_readonly(item.CategoryId, (level + 1), skipShoingId, ulclass, liClass, aHref, aClass);
 
                     res += string.Format("<a href=\"{0}\" class=\"{1}\" data-id=\"{2}\" data-name=\"{3}\" data-describe=\"{4}\" data-haveroot=\"{5}\" >{6}</a> {7}",
                         aHref, aClass, item.CategoryId, item.CategoryName,
                         HttpUtility.HtmlEncode(item.Description),
                         ((tmp.ToString().IsNullOrWhiteSpace()) ? 0 : 1),
                         item.CategoryName, tmp);
+                }
+                res += "</li>";
+            }
+
+
+            if (!res.IsNullOrWhiteSpace())
+                res = string.Format("<ul class=\"{0}\"  data-level=\"{1}\" >{2}</ul>", ulclass, level, res);
+            return new MvcHtmlString(res);
+        }
+        
+        
+        public MvcHtmlString Categories_managment(int? pid, int level = 1, string ulclass = "", string liClass = "", string aHref = "#", string aClass = "",
+            string editHref="#", string editClass = "", string editOnclick = "", string editText = "",
+            string removeHref = "#", string removeClass = "", string removeOnclick = "", string removeText = "")
+        {
+            string res = "";
+
+            foreach (var item in DBS.Categories.Where(c => c.ParentCategoryId == pid))
+            {
+                res += string.Format("<li class=\"{0}\" >", liClass);
+                {
+                    var tmp = Categories_managment(item.CategoryId, (level + 1), ulclass, liClass, aHref, aClass,
+                        editHref, editClass, editOnclick, editText,
+                        removeHref, removeClass, removeOnclick, removeText);
+
+                    res += string.Format("<a href=\"{0}\" class=\"{1}\" data-id=\"{2}\" data-name=\"{3}\" data-describe=\"{4}\" data-haveroot=\"{5}\" >{6}</a> {7} {8} {9}",
+                        aHref, aClass, item.CategoryId, item.CategoryName,
+                        HttpUtility.HtmlEncode(item.Description),
+                        ((tmp.ToString().IsNullOrWhiteSpace()) ? 0 : 1),
+
+                        item.CategoryName,
+
+                        string.Format("<a href=\"{0}\" class=\"{1}\" onclick=\"{2}('{3}','{4}','{5}')\">{6}</a>",
+                        editHref, editClass, editOnclick, item.CategoryId, item.CategoryName, item.Description, editText),
+                        string.Format("<a href=\"{0}\" class=\"{1}\" onclick=\"{2}('{3}','{4}','{5}')\">{6}</a>",
+                        removeHref, removeClass, removeOnclick, item.CategoryId, item.CategoryName, item.Description, removeText),
+
+                        tmp);
                 }
                 res += "</li>";
             }
