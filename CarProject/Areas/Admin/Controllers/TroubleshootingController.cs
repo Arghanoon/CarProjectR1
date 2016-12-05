2d656f5a4bb87ad6b035624d24469af90ddab0fb
mh828 <mh.hasanpour@gmail.com>
2016-12-05 12:37:48 +0330

    1395-09-15 12:37

--- CarProject/Areas/Admin/Controllers/TroubleshootingController.cs
+++ CarProject/Areas/Admin/Controllers/TroubleshootingController.cs
@@ -3,96 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using CarProject.App_extension;
-using CarProject.Areas.Admin.Models;
-using CarProject.CLS;
-
 
 namespace CarProject.Areas.Admin.Controllers
 {
-    [CarProject.CLS.AuthFilter]
+    //[CLS.AuthFilter]
     public class TroubleshootingController : Controller
     {
-        public ActionResult Index(int? parentID)
+        //
+        // GET: /Admin/Troubleshooting/
+
+        DBSEF.CarAutomationEntities DBS = new DBSEF.CarAutomationEntities();
+
+        public ActionResult Index()
         {
-            var xc = new DBSEF.CarAutomationEntities();
-            var xtcol = xc.Troubleshootings.Where(t => t.FatherId == parentID + 1).ToList();
-            if (parentID != null)
-            {
-                xtcol.AddRange(xc.Troubleshootings.Where(t => t.FatherId == parentID + 2).ToList());
-            }
-            return View(xtcol);
+            return View();
         }
-        // GET: Admin/Troubleshooting
-        public ActionResult New(int? id)
+
+        public ActionResult MakeAQuestion(int? Id)
         {
-            var tob = new Models.TroubleshootingClass();
-            if (id != null && id > 0)
+            var model = new Models.Troubleshooting.TSModel();
+            model.Troubleshooting.TroubleshootinParentId = Id;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult MakeAQuestion(Models.Troubleshooting.TSModel model)
+        {
+            if (ModelState.IsValid)
             {
-                DBSEF.CarAutomationEntities c = new DBSEF.CarAutomationEntities();
-                tob.Troubleshooting.FatherId = c.Troubleshootings.FirstOrDefault(t => t.TroubleshootingId == id).TroubleshootingId;
+                model.Save();
+                return RedirectToAction("DefineAnswer", new { id = model.Troubleshooting.TroubleshootingId });
             }
-            return View(tob);
+            return View(model);
+        }
+
+        public ActionResult DefineAnswer(int? Id)
+        {
+            if (DBS.Troubleshootings.Count(t => t.TroubleshootingId == Id) <= 0)
+                return RedirectToAction("Index");
+
+            var model = new Models.Troubleshooting.TSModel();
+            model.ModelType = Models.Troubleshooting.TSModel.TroubleshootingType.Answer;
+            model.Troubleshooting.TroubleshootinParentId = Id;
+            return View(model);
         }
         [HttpPost]
-        public ActionResult New(TroubleshootingClass tobj, TroubleshootingClass tobj1, TroubleshootingClass tobj2, FormCollection form)
+        public ActionResult DefineAnswer(int? Id,Models.Troubleshooting.TSModel model)
         {
-            int id = 0;
-            if (ViewData.ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                if (!tobj.Troubleshooting.QQuestion.IsNullOrWhiteSpace())
-                {
-                    if (tobj.Troubleshooting.FatherId.HasValue)
-                    {
-                        tobj.Troubleshooting.HasFather = true;
-                    }
-                    else
-                    {
-                        tobj.Troubleshooting.HasFather = false;
+                model.Save();
 
-                    }
-                    tobj.trb.Answer = tobj.Troubleshooting.QQuestion;
-                    tobj.Troubleshooting.QQuestion = null;
-                    tobj.Save();
-                }
-                if (!tobj.Troubleshooting.AnswerYes.IsNullOrWhiteSpace())
-                {
-                    tobj1.trb.FatherId = tobj.trb.TroubleshootingId;
-                    tobj1.trb.HasFather = false;
-                    tobj1.trb.Answer = tobj.Troubleshooting.AnswerYes;
-                    tobj1.Troubleshooting.AnswerYes = null;
-
-                    tobj1.Save();
-
-
-
-                }
-                if (!tobj.Troubleshooting.AnswerNo.IsNullOrWhiteSpace())
-                {
-                    tobj2.trb.FatherId = tobj.trb.TroubleshootingId;
-                    tobj2.trb.HasFather = false;
-                    tobj2.trb.Answer = tobj.Troubleshooting.AnswerNo;
-                    tobj2.Troubleshooting.AnswerNo = null;
-
-                    tobj2.Save();
-                }
-
-
-                if (form.AllKeys.Contains("saveOnly"))
-                {
-                    return RedirectToAction("Index", "Troubleshooting");
-                }
-                else if (form.AllKeys.Contains("saveAndContinueThisAsRoot"))
-                {
-                    return RedirectToAction("New", "Troubleshooting", new { id = tobj.Troubleshooting.TroubleshootingId });
-                }
-                else if (form.AllKeys.Contains("saveAndContinueToThisRoot"))
-                {
-                    return RedirectToAction("New", "Troubleshooting", new { id = tobj.Troubleshooting.FatherId });
-                }
+                return RedirectToAction("DefineAnswer", new { id = model.Troubleshooting.TroubleshootinParentId });
             }
-
-            return View(tobj);
+            return View(model);
         }
     }
 }
---
+++ CarProject/Areas/Admin/Models/Troubleshooting/TSModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using CarProject.App_extension;
+
+namespace CarProject.Areas.Admin.Models.Troubleshooting
+{
+    public class TSModel : IValidatableObject
+    {
+        public DBSEF.CarAutomationEntities dbs = new DBSEF.CarAutomationEntities();
+        public DBSEF.Troubleshooting Troubleshooting { get; set; }
+
+        public enum TroubleshootingType { Question = 0, Answer = 1 };
+
+        public TroubleshootingType ModelType { get; set; }
+
+        public List<int> Products { get; set; }
+
+        public TSModel()
+        {
+            Troubleshooting = new DBSEF.Troubleshooting();
+            ModelType = TroubleshootingType.Question;
+            Products = new List<int>();
+        }
+
+        public TSModel(int? id)
+        {
+            Troubleshooting = dbs.Troubleshootings.FirstOrDefault(t => t.TroubleshootingId == id);
+            if (Troubleshooting == null)
+                Troubleshooting = new DBSEF.Troubleshooting();
+
+            Products = new List<int>();
+            if (Troubleshooting.Products != null)
+            {
+                foreach (var item in Troubleshooting.Products)
+                {
+                    Products.Add(item.ProductId);
+                }
+            }
+        }
+
+        public void Save()
+        {
+            foreach (var item in Products)
+            {
+                Troubleshooting.Products.Add(dbs.Products.FirstOrDefault(p => p.ProductId == item));
+            }
+            dbs.Troubleshootings.Add(Troubleshooting);
+            dbs.SaveChanges();
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var res = new List<ValidationResult>();
+
+            if (ModelType == TroubleshootingType.Question)
+            {
+                if (Troubleshooting.Subject.IsNullOrWhiteSpace())
+                    res.Add(new ValidationResult("عنوان سوال تعیین نشده است", new string[] { "Troubleshooting.Subject" }));
+
+            }
+            else
+            {
+                if (Troubleshooting.Subject.IsNullOrWhiteSpace())
+                    res.Add(new ValidationResult("جواب مختصر تعیین نشده است", new string[] { "Troubleshooting.Subject" }));
+                if (Troubleshooting.Price.IsNullOrWhiteSpace())
+                    res.Add(new ValidationResult("هزینه تعمیرات / رفغ عیب تعیین نشده است", new string[] { "Troubleshooting.Price" }));
+                if(!Troubleshooting.Price.IsNumber())
+                    res.Add(new ValidationResult("مقدار وارد شده صحیح نیست", new string[] { "Troubleshooting.Price" }));
+            }
+            return res;
+        }
+    }
+}
