143caf09ff652ae7f7856a79b6bd9f4a43fb354c
mh828 <mh.hasanpour@gmail.com>
2016-12-06 18:34:15 +0330

    1395-09-16 18:34

---
+++ CarProject/Controllers/TroubleshootingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarProject.Controllers
+{
+    public class TroubleshootingController : Controller
+    {
+        //
+        // GET: /Troubleshooting/
+
+        public ActionResult Index(int? id)
+        {
+            return View();
+        }
+
+    }
+}
