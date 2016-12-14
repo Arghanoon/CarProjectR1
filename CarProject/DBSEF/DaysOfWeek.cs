f5dc122e3bfe18d96a3f5c3bfbe65aa6f8846be9
mh828 <mh.hasanpour@gmail.com>
2016-12-15 00:14:32 +0330

    1395-09-25 00:14

--- CarProject/DBSEF/DaysOfWeek.cs
+++ CarProject/DBSEF/DaysOfWeek.cs
@@ -17,6 +17,7 @@ namespace CarProject.DBSEF
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public DaysOfWeek()
         {
+            this.Baskets = new HashSet<Basket>();
             this.TimeOfDays = new HashSet<TimeOfDay>();
         }
     
@@ -25,6 +26,8 @@ namespace CarProject.DBSEF
         public string DayOfWeek { get; set; }
         public Nullable<bool> IsActive { get; set; }
     
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Basket> Baskets { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<TimeOfDay> TimeOfDays { get; set; }
     }
